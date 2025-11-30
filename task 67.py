dca8fecd2223583eef429a91e296e50252e37ad3
Beyza333 <beyzan.bozkir@gmail.com>
2025-11-30 11:19:51 +0300

    Update task 67.py

--- task 67.py
+++ task 67.py
@@ -1,10 +1,1 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 24 18:35:19 2025
 
-@author: BEYZA NUR
-"""
-
-def coordinator (x,y):
-    my_coordinates=coordinator(5,6)
-    print (my_coordinates)
